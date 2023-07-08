0f6de3cff79ef33e74e10fb24f75a1aadffb5af4
GoNz0 | Mn0g1as <128939384+Mn0g1as@users.noreply.github.com>
2023-07-08 18:38:17 +0200

    Update prankyoufriend.py

--- prankyoufriend.py
+++ prankyoufriend.py
@@ -1,1 +1,18 @@
+def main():
+  import webbrowser
+  import time
+  import random
+  websites = random.choice ([
+    "youtube.com",
+    "spotify.com",
+    "dropbox.com",
+    "uber.com",
+    "google.com",
+  ])
+  formatted = "https://{}".format (websites)
+  while 1:
+    webbrowser.open(formatted)
+    time.sleep(3)
 
+if __name__ == "__main__":
+  main()
